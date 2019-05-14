9dfdd20b9fc5456f67541c062ecdb00b030a0059
Adventure 2019 <caliew26@gmail.com>
2019-05-14 18:32:44 -0400

    added correct required packages

--- liri.js
+++ liri.js
@@ -1,8 +1,6 @@
-// require(".env").config();
+var env = require("dotenv").config();
 
-// var keys = require("./keys.js");
-
-// var spotify = new Spotify(keys.spotify);
+var keys = require("./keys.js");
 
 // concert-this
 
@@ -13,28 +11,30 @@
 // do-what-it-says
 
 var Spotify = require('node-spotify-api');
- 
-var spotify = new Spotify({
-  id: "0a9962848eeb4ab081315857f6efaa41",
-  secret: "47babb54a6c6498aba817f6929e90839",
-});
- 
-// search({ type: 'billy joel', query: 'Piano Man', limit: 1 });
+
+
+var spotify = new Spotify(keys.spotify);
+
+var input = process.argv[2];
+
+if(input === "concert-this"){
+    console.log("This is concert");
+}
 
 spotify
-  .request('https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx')
-  .then(function(data) {
+    .request('https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx')
+    .then(function(data) {
     console.log(data); 
-  })
-  .catch(function(err) {
+    })
+    .catch(function(err) {
     console.error('Error occurred: ' + err); 
-  });
-
-// spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-//   if (err) {
-//     return console.log('Error occurred: ' + err);
-//   }
- 
-
-// console.log(data); 
-// });
+});
+  
+spotify
+    .search({ type: 'album', query: 'A New Day', limit: 1})
+    .then(function(response) {
+    console.log(response);
+    })
+    .catch(function(err) {
+    console.log(err);
+});
