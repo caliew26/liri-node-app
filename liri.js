997580cd15b1c5db3d7cccf1bc83e39bacee94b6
Adventure 2019 <caliew26@gmail.com>
2019-05-15 20:16:40 -0400

    adding do-what-it-says logic for random.txt file

--- liri.js
+++ liri.js
@@ -6,49 +6,59 @@ var Spotify = require('node-spotify-api');
 
 var axios = require("axios");
 
-var input = process.argv[2];
-if(process.argv[2]=== 'concert-this'){
-    function myConcert(input) {
-    var concert = userInput;
+var fs = require("fs");
 
-    if(!concert){
-        concert = "New Kids on The Block";
+var input = process.argv[2];
+
+if(process.argv[2]=== 'concert-this'){
+    // function myConcert(input) {
+    var concertBand = "";
+
+    if(!concertBand){
+        concertBand = "New Kids on The Block";
         }
         // console.log("This is concert");
         
-        var url = "https://rest.bandsintown.com/artists/" + concert + "/events?app_id=codingbootcamp";
-        
+        var queryUrl = "https://rest.bandsintown.com/artists/" + concertBand + "/events?app_id=codingbootcamp";
+
+        axios.get(queryUrl).then(function(response){
+            console.log("Date of concert " + response.datetime);
+            console.log("Date tickets go on sale " + response.on_sale_datetime);
+        })
 };
-};
+
 
 var spotify = new Spotify(keys.spotify);
 
 if(process.argv[2]==="spotify-this-song"){
-    function myMusic(input){
-        var song = userInput;
+    // function myMusic(input){
+        var songName = "";
 
-        if(!song){
-            song = "Let It Go"
+        if(!songName){
+            songName = "Let It Go"
             // console.log("This is a song");
         };
             
         spotify.search({ 
                 type: 'track', 
-                query: song, 
-                limit: 1,
+                query: songName, 
+                limit: 3,
             },
             function(err, response) {
                 // console.log("this is a test");
                 if(err){
                 console.log("The error " + err)
             }else{
-            console.log("Song name " + response.tracks.items[0].name);
-            console.log("Artist " + response.tracks.artists[0].name);
-            console.log("Album " + response.tracks.album[0].name);
+                console.log("response " + response);
+                console.log("name " + response.tracks.album.name);
+                console.log("type " + response.tracks.artists.type);
+                console.log("artist " + response.tracks.artists.name);
+                // console.log("Song name " + response.tracks.item);
+                // console.log("Artist " + response.tracks.album.artists[0]);
+                // console.log("Album " + response.tracks.album.name);
             };
         });
     };
-}
 
 if(process.argv[2]==="movie-this"){
     // function myMovie(userInput){
@@ -72,10 +82,14 @@ if(process.argv[2]==="movie-this"){
 // };
 
 if(process.argv[2]==="do-what-it-says"){
-    function myBoss(input){
+    // function myBoss(input){
     if(input === "do-what-it-says"){
         console.log("Do it!");
-        //will figure out what happens here
+        };
+    fs.readFile("random.txt", "utf8", function(error,data){
+        if (error){
+            console.log(error);
         }
-    };
+    console.log(data);
+    })
 };
